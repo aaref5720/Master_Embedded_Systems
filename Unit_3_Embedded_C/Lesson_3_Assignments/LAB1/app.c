7666919a5212ade603929ed77ce7898862b3c7e2
Abdelrahman Aref <66727224+aaref5720@users.noreply.github.com>
2022-10-25 01:13:01 +0200

    Delete Unit_3_Embedded_C/Lesson_3_Assignments/LAB1 directory

--- Unit_3_Embedded_C/Lesson_3_Assignments/LAB1/app.c
+++
@@ -1,9 +0,0 @@
-#include "uart.h"
-
-unsigned char string_buffer1[100] = "learn-in-depth:Abdelrahman";
-unsigned const char string_buffer2[100] = "learn-in-depth:Abdelrahman";
-
-void main(void)
-{
-	Uart_Send_String(string_buffer1);
-}
